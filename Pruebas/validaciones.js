3fa65a03430bcdaccb85e60adcd2ba676eee4399
Daniel Cortes <danielcortes026@gmail.com>
2021-09-22 13:10:24 -0500

    Carpeta de Pruebas - condicionales

---
+++ Pruebas/validaciones.js
@@ -0,0 +1,45 @@
+/*alert("Hola, desde codynamico")
+
+
+function validar_nombre_usuario(string) {
+    
+}
+
+function validar_edad_usuario (edad) {
+
+}
+
+/* >=6 Caracteres */
+/* Alfanumerico de de la A a la Z,o, 0 al 9 "No combinados" */
+/* retorna verdadero si cumple condiciones, si no, falso 
+
+function validar_contrasena (string) {
+    dato_contrasena = 0
+    if (dato_contrasena);
+}
+*/
+
+
+function calcularedad (){
+    var edad = document.getElementById("edad").value;
+
+    if (edad == 18) {
+        alert("Esta canal es permitido para ti")
+    }else {
+        alert("Prohivido para ti")
+    }
+
+}
+
+
+
+
+/*
+var edad = 14;
+
+if (edad == 18) {
+    alert("Esta canal es permitido para ti")
+}else {
+    alert("Prohivido para ti")
+}
+*/
